b7f623c7afd8b2edc74ce563a8aa6aa3f6156c2e
영하 양 <dakmuk@gmail.com>
2022-06-17 23:44:25 +0900

    1. Install ESLint 2. Modify configs

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     path: __dirname + '/dist',
     filename: 'index_bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin()
-  ]
+  ],
+  performance: {
+      hints: false,
+      maxEntrypointSize: 51200,
+      maxAssetSize: 51200
+  }
 };
